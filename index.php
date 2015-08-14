394458b71ca3e8860be796a789cad64378d79c8e
Christian Gloss <cgloss@stansberryresearch.com>
2015-08-14 14:38:49 -0400

    initial

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include('mq.php');
+?>
+<!DOCTYPE html>
+<head>
+<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width" />
+<link rel="stylesheet" href="test.css" type="text/css" media="all" />
+<title>MoaR Queries</title>
+</head>
+<body>
+derp
+</body>
+</html>
